924b506dd283d6eebfe5fcc38f5a35a4439e3285
Ferdi <Mia Ferdi akson>
2026-06-05 02:18:05 +0800

    fix: revert BASE_URL to static value for API integration

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://trushbin.my.id/api";
+const BASE_URL = "https://trushbin.my.id/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
